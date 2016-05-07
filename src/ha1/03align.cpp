15e50e60316a6eb14cd5e566fdd893cb5b863852
friebetill <friebe@campus.tu-berlin.de>
2016-05-07 14:23:43 +0200

    Work on 03allign.cpp

--- src/ha1/03align.cpp
+++ src/ha1/03align.cpp
@@ -5,6 +5,7 @@
 
 
 
+float32[] ranges;
 
 
 void sensorCallback(const create_fundamentals::SensorPacket::ConstPtr& msg)
@@ -14,29 +15,28 @@ void sensorCallback(const create_fundamentals::SensorPacket::ConstPtr& msg)
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-  ROS_INFO("%f", msg->ranges[msg->ranges.size()/2]);
+  ranges = msg->ranges;
+}
 
-  float min = msg->range_max;
+/* *
+ * Recognise a wall with ransac
+ * Returns two points of the wall
+ * */
+float32[] ransac()
+{
+  int iterations = 1000
+  for (i = 0; i < iterations; ++i) {
+    std::srand(123456789);
+    firstRandom = std::rand();
+    secondRandom = std::rand();
 
-  for (int i = 0; i < msg->ranges.size(); ++i) {
-    if (msg->ranges[i] < min) {
-      min = msg->ranges[i];
-    }
-  }
+    a = ranges[firstRandom]
 
 
-  if (min < 0.2) {
-    ROS_INFO("Avoid Crash.");
-    soft_stop(stopped);
-    change_direction();
-  } else {
-    ROS_INFO("diffDrive 10 10");
-    ::srv.request.left = SPEED;
-    ::srv.request.right = SPEED;
-    ::diffDrive.call(::srv);
-    stopped = 0;
+
   }
 }
+
 
 int main(int argc, char **argv)
 {
@@ -60,6 +60,10 @@ int main(int argc, char **argv)
   while (n.ok()) {
     // Publish the message
     node_example->publishMessage(&pub_message);
+
+    if (recWall() == true) {
+
+    }
 
     ros::spinOnce();
     r.sleep();
