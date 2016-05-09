5a58c035e333b00b425cb1dd2712ae54640c0f76
friebetill <friebe@campus.tu-berlin.de>
2016-05-09 21:06:31 +0200

    HA1 A3: Working on angleCalculation
    Start working on angle calculation from robot center to wall.

--- src/ha1/03align.cpp
+++ src/ha1/03align.cpp
@@ -9,7 +9,7 @@
 
 #define SPEED 10 // target speed of robot (straigt)
 #define RATE 5
-#define DISTANCE_LASER_TO_ROBOT_CENTER -0.125
+#define DISTANCE_LASER_TO_ROBOT_CENTER 0.125
 #define ONE_METER_IN_RAD 30.798
 
 int cur_speed = 0; // in 10th parts (zehntel) of SPEED, does not apply to truning
@@ -24,15 +24,14 @@ ros::ServiceClient reset_enc;
 
 void resetEncoders(void)
 {
-        reset_enc.call(srv_renc);
-        left_encoder = right_encoder = 0;
+  reset_enc.call(srv_renc);
+  left_encoder = right_encoder = 0;
 }
 
 void sensorCallback(const create_fundamentals::SensorPacket::ConstPtr& msg)
 {
-        left_encoder = msg->encoderLeft;
-        right_encoder = msg->encoderRight;
-  //ROS_INFO("left encoder: %f, right encoder: %f", msg->encoderLeft, msg->encoderRight);
+  left_encoder = msg->encoderLeft;
+  right_encoder = msg->encoderRight;
 }
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
@@ -51,62 +50,76 @@ void stop() {
   ::srv.request.left = 0;
   ::srv.request.right = 0;
   ::diffDrive.call(::srv);
-
 }
 
-float calculateAngleToWall(std::vector<float> wall, float distance)
+float calculateAngleToWallFromLaser(std::vector<float> wall, float distance)
 {
   float m = (wall[3] - wall[1]) / (wall[2] - wall[0]);
   float n = wall[1] - m * wall[0];
 
   //float angle = atan2(wall[3] - wall[1], wall[2] - wall[0]);
-  float angle = asin(distance/n);
+  float angle = asin((distance)/n);
 
   if(m > 0) {
     angle = fabs(asin(distance/n) - PI);
   }
 
   return angle;
+}
+
+float calculateAngleToWallFromRobot(float angle, float distance)
+{
+  float a = cos(angle) * distance;
+  float b = sin(angle) * distance - DISTANCE_LASER_TO_ROBOT_CENTER;
+  float c = sqrt(pow(a, 2) + pow(b, 2));
+  float beta = acos(a/c);
+  ROS_INFO("Gamma = %f", acos(a/c));
+
+  // m_wall = (wall[3] - wall[1])/(wall[2] - wall[0])
+  // n_wall =
+
+  return beta;
+}
 
 void wallAlign(float angle_in_rad)
 {
-        resetEncoders();
-        ros::Rate loop_rate(16);
-        ros::spinOnce();
+  resetEncoders();
+  ros::Rate loop_rate(16);
+  ros::spinOnce();
 
-        // rotate left if `actual_angel' is greater than 0
-        // rotate right otherwise
-        float actual_angle = angle_in_rad - PI/2;
-        int direction = (actual_angle > 0) ? 1 : -1;
-        if(actual_angle < 0) actual_angle *= -1;
-        float threshold =
-                actual_angle - actual_angle * ONE_METER_IN_RAD * 0.13;
+  // rotate left if `actual_angel' is greater than 0
+  // rotate right otherwise
+  float actual_angle = angle_in_rad - PI/2;
+  int direction = (actual_angle > 0) ? 1 : -1;
+  if(actual_angle < 0) actual_angle *= -1;
+  float threshold = (actual_angle - actual_angle * 0.04) * ONE_METER_IN_RAD * 0.13;
+  ROS_INFO("Treshold = %f", threshold);
 
-        float speed_left_r = 2.5 * direction;
-        float speed_right_r = 2.5 * direction * -1;
+  float speed_left_r = 2.5 * direction;
+  float speed_right_r = 2.5 * direction * -1;
 
-        while(ros::ok()) {
+  while(ros::ok()) {
 
-                if((speed_right_r == 0) && (speed_left_r == 0)) break;
+    if((speed_right_r == 0) && (speed_left_r == 0)) break;
 
-                if(((right_encoder >= threshold) && (right_encoder > 0)) ||
-                   (((right_encoder*(-1) >= threshold)) && (right_encoder < 0)))
+    if(((right_encoder >= threshold) && (right_encoder > 0)) ||
+       (((right_encoder*(-1) >= threshold)) && (right_encoder < 0)))
 
-                        speed_right_r = 0;
+      speed_right_r = 0;
 
-                if(((left_encoder >= threshold) && (left_encoder > 0)) ||
-                   (((left_encoder*(-1) >= threshold)) && (left_encoder < 0)))
+    if(((left_encoder >= threshold) && (left_encoder > 0)) ||
+       (((left_encoder*(-1) >= threshold)) && (left_encoder < 0)))
 
-                        speed_left_r = 0;
+      speed_left_r = 0;
 
-                srv.request.left = speed_left_r;
-                srv.request.right = speed_right_r;
-                diffDrive.call(srv);
-                ros::spinOnce();
-                loop_rate.sleep();
-        }
+    srv.request.left = speed_left_r;
+    srv.request.right = speed_right_r;
+    diffDrive.call(srv);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
 
-        resetEncoders();
+  resetEncoders();
 }
 
 int main(int argc, char **argv)
@@ -134,19 +147,28 @@ int main(int argc, char **argv)
 
     ros::spinOnce();
 
-    float distance;
-    float angle;
+    float distanceFromLaser;
+    float angleFromRobot;
+    float angleFromLaser;
     wall = ransac();
+
     // Check if wall was found with ransac
     if(!isnan(wall[0])){
       // Wall was found
-      distance = distanceFromLineToPoint(wall[0], wall[1], wall[2], wall[3], 0, 0);
-      angle = calculateAngleToWall(wall, distance);
-      ROS_INFO("Distance = %f", distance);
-      ROS_INFO("Angle: %f", angle*180/3.1416);
-    }
+      distanceFromLaser = distanceFromLineToPoint(wall[0], wall[1], wall[2], wall[3], 0, 0);
+      angleFromLaser = calculateAngleToWallFromLaser(wall, distanceFromLaser);
+      angleFromRobot = calculateAngleToWallFromRobot(angleFromLaser, distanceFromLaser);
+      ROS_INFO("Distance = %f", distanceFromLaser);
+      ROS_INFO("AngleFromLaser: %f", angleFromLaser*180/3.1416);
+      ROS_INFO("AngleFromRobot: %f", angleFromRobot*180/3.1416);
 
+      //wallAlign(angle);
+      //for(int i = 0; i < 50; i++){
+      //  r.sleep();
+      //}
+    }
     r.sleep();
+
   }
 
   return 0;
