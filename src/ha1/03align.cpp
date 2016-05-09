76b41cbb708da572ffc4fa36776925be5fb233eb
Rodrigo Bermudez Schettino <rbermudez10@gmail.com>
2016-05-09 18:25:37 +0200

    HA1 A3: Calculation of angle (beta)
    Angle calculation is not working as expected.

--- src/ha1/03align.cpp
+++ src/ha1/03align.cpp
@@ -68,13 +68,18 @@ int main(int argc, char **argv)
     // Check if wall was found with ransac
     if(!isnan(wall[0])){
       // Wall was found
-      //distance = distanceFromLineToPoint(wall[0], wall[1], wall[2], wall[3], 0, DISTANCE_LASER_TO_ROBOT_CENTER);
       distance = distanceFromLineToPoint(wall[0], wall[1], wall[2], wall[3], 0, 0);
       ROS_INFO("Distance = %f", distance);
+      ROS_INFO("ax = %f, ay = %f", wall[0], wall[1]);
+      ROS_INFO("bx = %f, by = %f", wall[2], wall[3]);
+      float m = (wall[3] - wall[1]) / (wall[2] - wall[0]);
+      float n = wall[1] - m * wall[0];
+      float angle = asin(distance/n);
+      ROS_INFO("m: %f");
+      // ROS_INFO("angle: %f", angle*180/3.1416); 
+      // ROS_INFO("angle %f ", (wall[3]-wall[1])/(wall[2]-wall[0])*(0-wall[0]) + wall[1]);
     }
 
-    ROS_INFO("ax = %f, ay = %f", wall[0], wall[1]);
-    ROS_INFO("bx = %f, by = %f", wall[2], wall[3]);
 
     r.sleep();
   }
