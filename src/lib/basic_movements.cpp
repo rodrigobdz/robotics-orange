55e43a68ef12abac94ce778f9edb207809b683e7
Rodrigo Bermudez Schettino <rbermudez10@gmail.com>
2016-05-10 22:35:35 +0200

    Lib: Stop function implemented
    Function was taken from example_moving.cpp.
    Node was initialized in function and was not given as parameter. Approach has to be discussed.

--- src/lib/basic_movements.cpp
+++ src/lib/basic_movements.cpp
@@ -3,16 +3,25 @@
 #include "create_fundamentals/DiffDrive.h"
 #include "create_fundamentals/SensorPacket.h"
 
-// Distances are given in meters
 #define MAXIMUM_SPEED 10
+#define ONE_METER_IN_RAD 30.798
+// Distances are given in meters
 #define SAFETY_DIS 0.15 // Minimum distance to keep when driving
 #define CELL_LENGTH 0.80 
-#define ONE_METER_IN_RAD 30.798
-#define DEBUG false
+#define DEBUG false // Constant to define if output should be printed
 
 void stop() 
 {
-  
+  ros::init(argc, argv, "stop");
+  ros::NodeHandle n;
+  ros::ServiceClient diffDrive = n.serviceClient<create_fundamentals::DiffDrive>("diff_drive");
+  create_fundamentals::DiffDrive srv;
+
+  if(DEBUG) ROS_INFO("diffDrive 0 0");
+
+  srv.request.left  = 0;
+  srv.request.right = 0;
+  diffDrive.call(srv);
 }
 
 /*
