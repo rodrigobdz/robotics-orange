fa1addf03c5687623e7050cec855b9c33ed8f984
Rodrigo Bermudez Schettino <rbermudez10@gmail.com>
2016-05-10 22:28:11 +0200

    Lib: Constants defined and needed files included
    Function parameters specified for driveForward and driveBackward.
    Default return values added to functions.

--- src/lib/basic_movements.cpp
+++ src/lib/basic_movements.cpp
@@ -1,23 +1,36 @@
+#include "ros/ros.h"
+#include <cstdlib>
+#include "create_fundamentals/DiffDrive.h"
+#include "create_fundamentals/SensorPacket.h"
+
+// Distances are given in meters
+#define MAXIMUM_SPEED 10
+#define SAFETY_DIS 0.15 // Minimum distance to keep when driving
+#define CELL_LENGTH 0.80 
+#define ONE_METER_IN_RAD 30.798
+#define DEBUG false
 
 void stop() 
 {
-
+  
 }
 
 /*
- * Returns false if obstacle was found otherwise true
+ * Params: distance in meter
+ * Returns: false if obstacle was found otherwise true
 **/
 bool driveForward(float distance) 
 {
-
+  return true;
 }
 
 /*
+ * Params: distance in meter
  * Returns false if obstacle was found otherwise true
 **/
 bool driveBackwards(float distance) 
 {
-
+  return true;
 }
 
 void rotateLeft(float angle) 
