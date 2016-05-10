e937c23ad0a0861d46a82cf5eb2f63daf8f963c7
Rodrigo Bermudez Schettino <rbermudez10@gmail.com>
2016-05-10 22:06:03 +0200

    Lib: Empty function declarations
    Function names were given and return types as well.

---
+++ src/lib/basic_movements.cpp
@@ -0,0 +1,31 @@
+
+void stop() 
+{
+
+}
+
+/*
+ * Returns false if obstacle was found otherwise true
+**/
+bool driveForward(float distance) 
+{
+
+}
+
+/*
+ * Returns false if obstacle was found otherwise true
+**/
+bool driveBackwards(float distance) 
+{
+
+}
+
+void rotateLeft(float angle) 
+{
+
+}
+
+void rotateRight(float angle)
+{
+  
+}
