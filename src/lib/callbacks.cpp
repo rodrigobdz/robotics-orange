ae3c296ef222cd16358b09558c7caca66461a768
Rodrigo Bermudez Schettino <rbermudez10@gmail.com>
2016-05-10 23:50:03 +0200

    Lib: Callbacks file created with empty function declarations
    This file contains the callback functions and some helpers for encoders and laser scanner.

---
+++ src/lib/callbacks.cpp
@@ -0,0 +1,22 @@
+
+namespace callbacks {
+	void encoderCallback(const create_fundamentals::SensorPacket::ConstPtr& msg)
+	{
+	  
+	}
+
+	void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+	{
+	  
+	}
+}
+
+namespace helpers {
+  /*
+   * Reset the encoders and corresponding helper values.
+  **/
+  void resetEncoders()
+  {
+
+  }
+}
