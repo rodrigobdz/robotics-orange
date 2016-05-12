3eb30ff3d456014c43a2dcbf556bb2fc6ed37d7f
Rodrigo Bermudez Schettino <rbermudez10@gmail.com>
2016-05-12 18:31:24 +0200

    Lib basic movements: From namespace to class
    Namespace is no longer used and class created instead.
    Class initializer with all callbacks created.

--- src/lib/basic_movements.cpp
+++ src/lib/basic_movements.cpp
@@ -1,104 +1,116 @@
 // Needed includes for this library to work
-// #include "ros/ros.h"
-// #include <cstdlib>
-// #include "create_fundamentals/DiffDrive.h"
-// #include "create_fundamentals/SensorPacket.h"
-// #include "sensor_msgs/LaserScan.h"
+#include "ros/ros.h"
+#include <cstdlib>
+#include "create_fundamentals/DiffDrive.h"
+#include "create_fundamentals/SensorPacket.h"
+#include "sensor_msgs/LaserScan.h"
 #include "create_fundamentals/ResetEncoders.h"
-#include "callbacks.cpp"
 
-#define MAXIMUM_SPEED 10
-#define ONE_METER_IN_RAD 30.798
-// Distances are given in meters
-#define SAFETY_DIS 0.15 // Minimum distance to keep when driving
-#define CELL_LENGTH 0.80 
-#define DEBUG false // Defines if output should be printed
-#define LOOP_RATE 16 // Used for loop rate
 
-namespace basic_movements {
-  float leftEncoder;
-  float rightEncoder;
 
-  void stop(create_fundamentals::DiffDrive srv, ros::ServiceClient diffDrive) 
-  {
-    if(DEBUG) {
-      ROS_INFO("diffDrive 0 0");
+class BasicMovements 
+{
+  public: 
+    BasicMovements()
+    {
+      // Set up encoders callback
+      encoderSubscriber = n.subscribe("sensor_packet", 1, &BasicMovements::encoderCallback, this);
+      diffDriveClient = n.serviceClient<create_fundamentals::DiffDrive>("diff_drive");
+
+      // Set up laser callback
+      laserSubscriber = n.subscribe("scan_filtered", 1, &BasicMovements::laserCallback, this);
+
+      // Set up reset encoders client
+      resetEncodersClient = n.serviceClient<create_fundamentals::ResetEncoders>("reset_encoders");
     }
 
-    srv.request.left  = 0;
-    srv.request.right = 0;
-    diffDrive.call(srv);
-  }
+    void stop();
+    bool drive(float distance);
+    bool rotate(float angle);
+  private: 
+    static const int MAXIMUM_SPEED      = 10;
+    static const int MEDIUM_SPEED       = 3;
+    static const float ONE_METER_IN_RAD = 30.798;
+    // Distances are given in meters
+    static const float SAFETY_DIS       = 0.15; // Minimum distance to keep when driving
+    static const float CELL_LENGTH      = 0.80; 
+    static const bool DEBUG             = false; // Defines if output should be printed
+    static const int LOOP_RATE          = 16; // Used for loop rate
 
-  /*
-   * Params: distance in meter
-   * Returns: false if obstacle was found otherwise true
-  **/
-  bool driveForward(ros::NodeHandle n, ros::ServiceClient diffDrive, create_fundamentals::DiffDrive srv, float distance) 
-  {
 
-    // Subscribe to laser scanner
-    ros::NodeHandle laser;
-    ros::Subscriber subLaser = laser.subscribe("scan_filtered", 1, callbacks::laserCallback);
-    
-    // Subscribe to reset encoders
-    ros::ServiceClient resetEncodersClient;
+    ros::NodeHandle n;
+    // Encoders
+    ros::Subscriber encoderSubscriber;
+    // Differential Drive
+    create_fundamentals::DiffDrive diffDriveService;
+    ros::ServiceClient diffDriveClient;
+    // Reset Encoders
     create_fundamentals::ResetEncoders resetEncodersService;
-    resetEncodersClient = n.serviceClient<create_fundamentals::ResetEncoders>("reset_encoders");
+    ros::ServiceClient resetEncodersClient;
+    // Laser
+    ros::Subscriber laserSubscriber;
 
-    // Subscribe to encoder
-    ros::Subscriber subEncoder = n.subscribe("sensor_packet", 1, callbacks::encoderCallback);
 
-    // TODO: Check if obstacle is in front and return if yes
+    std::vector<float> ranges;
+    float leftEncoder, rightEncoder;
 
-    float threshold  = ONE_METER_IN_RAD - (ONE_METER_IN_RAD*0.025);
-    float speedLeft  = 3;
-    float speedRight = 3;
 
-    helpers::resetEncoders(resetEncodersClient, resetEncodersService);
-    ros::Rate loop_rate(LOOP_RATE);
-    
-    while(ros::ok()) {
-      // TODO: Check if obstacle is in front
-      if ((speedRight == 0) && (speedLeft == 0)) break;
+    void encoderCallback(const create_fundamentals::SensorPacket::ConstPtr& msg);
+    void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
+    void resetEncoders();
+};
 
-      if (rightEncoder >= threshold) {
-        speedRight = 0;
-      }
 
-      if (leftEncoder >= threshold) {
-        speedLeft = 0;
-      }
 
-      srv.request.left  = speedLeft;
-      srv.request.right = speedRight;
-      diffDrive.call(srv);
-
-      ros::spinOnce();
-      loop_rate.sleep();
-    }
-
-    helpers::resetEncoders(resetEncodersClient, resetEncodersService);
-
-    return true;
+void BasicMovements::stop()
+{
+  if(DEBUG) {
+    ROS_INFO("diffDrive 0 0");
   }
 
-  /*
-   * Params: distance in meter
-   * Returns false if obstacle was found otherwise true
-  **/
-  bool driveBackwards(float distance) 
-  {
-    return true;
-  }
+  diffDriveService.request.left  = 0;
+  diffDriveService.request.right = 0;
+  diffDriveClient.call(diffDriveService);
+}
 
-  void rotateLeft(float angle) 
-  {
+/*
+ * Params: distance in meter. If negative distance robot
+ *         will go backwards
+ * Returns: false if obstacle was found otherwise true
+**/
+bool BasicMovements::drive(float distance) 
+{
+  return true;
+}
 
-  }
+/*
+ * Params: if angle positive robot will rotate clockwise
+ *         else if negative counter clockwise
+**/
+bool BasicMovements::rotate(float angle)
+{
+  return true; 
+}
 
-  void rotateRight(float angle)
-  {
-    
-  }
+
+
+/********************** HELPERS *****************************/
+
+
+
+void BasicMovements::encoderCallback(const create_fundamentals::SensorPacket::ConstPtr& msg)
+{
+  
+}
+
+void BasicMovements::laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+  
+}
+
+/*
+ * Reset the encoders and corresponding helper values.
+**/
+void BasicMovements::resetEncoders() {
+
 }
