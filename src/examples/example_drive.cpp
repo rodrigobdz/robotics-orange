743973fa6eeb1e556495fb0f991c2f68e39bd32b
friebetill <friebe@campus.tu-berlin.de>
2016-05-18 10:58:28 +0200

    Example: Added example_ransac for ransac example.

--- src/examples/example_drive.cpp
+++ src/examples/example_drive.cpp
@@ -1,10 +1,10 @@
 #include "../lib/basic_movements.cpp"
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "example_drive");
+    ros::init(argc, argv, "example_drive");
 
-	BasicMovements basicMovements;
-	// Drive one meter with a velocity of 6 rad/s
-	basicMovements.rotate(1,6);
+    BasicMovements basicMovements;
+    // Drive one meter with a velocity of 6 rad/s
+    basicMovements.rotate(1, 6);
 }
