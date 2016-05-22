04bcef52cc6b3ac4c4a46c80e74456ebc36dbe01
friebetill <friebe@campus.tu-berlin.de>
2016-05-22 17:13:28 +0200

    HA2 A1: Moving with serviceCall works unprecise
    The serviceCall works and the robot turns in the right directions.
    But the rotate and drive function is unprecise.

--- src/ha2/01moving_through_the_grid.cpp
+++ src/ha2/01moving_through_the_grid.cpp
@@ -1,28 +1,95 @@
 
 #include "ros/ros.h"
 #include "orange_fundamentals/ExecutePlan.h"
+#include <basic_movements.cpp>
 
 enum directions { RIGHT = 0, UP, LEFT, DOWN };
+
 
 bool execute_plan_callback(orange_fundamentals::ExecutePlan::Request& req,
                            orange_fundamentals::ExecutePlan::Response& res)
 {
+    BasicMovements basicMovements;
     std::vector<int> plan = req.plan;
+    int lastDirection = UP;
     for (std::vector<int>::iterator it = plan.begin(); it != plan.end(); ++it) {
         ROS_INFO("execute_plan_callback: %d", *it);
 
-        switch (*it) {
+        ROS_INFO("Drive in %i", *it);
+        switch (lastDirection) {
         case RIGHT:
+            switch (*it) {
+            case RIGHT:
+                break;
+            case UP:
+                basicMovements.rotate(90);
+                break;
+            case LEFT:
+                basicMovements.rotate(180);
+                break;
+            case DOWN:
+                basicMovements.rotate(-90);
+                break;
+            default:
+                break;
+            }
             break;
         case UP:
+            switch (*it) {
+            case RIGHT:
+                basicMovements.rotate(-90);
+                break;
+            case UP:
+                break;
+            case LEFT:
+                basicMovements.rotate(90);
+                break;
+            case DOWN:
+                basicMovements.rotate(180);
+                break;
+            default:
+                break;
+            }
             break;
         case LEFT:
+            switch (*it) {
+            case RIGHT:
+                basicMovements.rotate(180);
+                break;
+            case UP:
+                basicMovements.rotate(-90);
+                break;
+            case LEFT:
+                break;
+            case DOWN:
+                basicMovements.rotate(90);
+                break;
+            default:
+                break;
+            }
             break;
         case DOWN:
+            switch (*it) {
+            case RIGHT:
+                basicMovements.rotate(90);
+                break;
+            case UP:
+                basicMovements.rotate(180);
+                break;
+            case LEFT:
+                basicMovements.rotate(-90);
+                break;
+            case DOWN:
+                break;
+            default:
+                break;
+            }
             break;
         default:
             break;
         }
+        basicMovements.drive(0.8);
+        lastDirection = *it;
     }
     res.success = true;
     return true;
