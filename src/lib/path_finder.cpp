93b64cce5613c56020acd861de98193f4b50c0d0
Rodrigo Bermudez Schettino <rbermudez10@gmail.com>
2016-06-22 21:55:08 +0200

    Path finder: Create empty functions for structure
    Skeleton created where functions have to be implemented.

--- src/lib/path_finder.cpp
+++ src/lib/path_finder.cpp
@@ -25,28 +25,60 @@ class PathFinder
         // Variables
         bool DEBUG = true;
         std::vector<Row> rows; // Map variable
-        std::vector<std::vector<int>> weightedRows;
+        std::vector<std::vector<int>> weightedMap;
         
         // External libraries
 
         // Functions
-        void initializeFloodMap(Position start);
+        void initializeWeightedMap(Position);
+        void setDistancesInWeightedMap(Position);
+        std::vector<Position> getNeighbours(Position);
 };
 
 std::vector<int> PathFinder::find(Position start, Position end)
 {
+    if(DEBUG) {
+        ROS_INFO("PathFinder find start: %d end: %d", start.getXCoordinate(), start.getYCoordinate());
+    }
     // Initialize weights in map starting from given position
-    initializeFloodMap(start);
+    initializeWeightedMap(start);
+    setDistancesInWeightedMap(start);
+
 }
 
-void PathFinder::initializeFloodMap(Position start)
+void PathFinder::initializeWeightedMap(Position start)
 {
-    for(int x; x < rows.size(); x++) {
-        for(int y; y < rows[x].cells.size(); y++) {
-            
+    for(int y; y < rows.size(); y++) {
+        for(int x; x < rows[y].cells.size(); x++) {
+            if(y == start.getYCoordinate() && x == start.getXCoordinate()) {
+                weightedMap[x][y] = 0;
+                continue;
+            }
+            weightedMap[x][y] = MAX_INT;
         }
     }
+}
 
+void PathFinder::setDistancesInWeightedMap(Position start)
+{
+    std::vector<Position> positionsBacklog;
+    getNeighbours(start);
+}
+
+std::vector<Position> PathFinder::getNeighbours(Position position)
+{
+    std::vector<int> walls = rows[position.getYCoordinate()].cells[position.getXCoordinate()].walls;
+    // Loop through all possible orientations to
+    // discover if a neighbor is accessible or not.
+    for (int i = 0; i < 4; i++) {
+        // switch (walls[]) {
+
+        // }
+    }
+
+    if (true) {
+
+    }
 }
 
 #endif // PATH_FINDER_LIB
