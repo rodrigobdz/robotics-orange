b109950a98983b303aad0c977d337deab185a7c9
Rodrigo Bermudez Schettino <rbermudez10@gmail.com>
2016-06-22 18:18:04 +0200

    Path finder: Add example file
    Example file shows usage and serves as test.

---
+++ src/examples/example_pathfinder.cpp
@@ -0,0 +1,20 @@
+#include <signal.h>
+#include <path_finder.cpp>
+#include <basic_movements.cpp>
+
+void stopMotors(int signal) {
+    BasicMovements basic_movements;
+    basic_movements.stop();
+    ros::shutdown();
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "example_align");
+  signal(SIGINT, stopMotors);
+  
+  PathFinder pathFinder;
+  // pathFinder.find();
+
+  return 0;
+}
